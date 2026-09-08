602f3c372e51982bb3a895b176c275ccb2eb7cfa
agleveratto <agleveratto@gmail.com>
2026-09-08 17:01:41 -0300

    feat: initialize project skeleton with base Maven configuration and main test
    - Add pom.xml configured for Java 21 and Spring Boot 3.2.3.
    - Add CalculatorApplication main class in com.tenpo.calculator.
    - Add contextLoads test in CalculatorApplicationTest.
    - Configure .gitignore to ignore IDE files (.idea) and build artifacts (target/).

---
+++ src/test/java/com/tenpo/calculator/CalculatorApplicationTest.java
@@ -0,0 +1,12 @@
+package com.tenpo.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
